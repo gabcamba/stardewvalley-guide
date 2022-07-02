081fbbd2e8518ff1b5c76dfd3801be8cca843783
Gab Camba <gabcamba@outlook.com>
2022-07-02 22:20:21 +0800

    - added image URIs for birthdays

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const DARK_MODE_COLOR = '#212124'
+export const WHITE_COLOR = '#ffffff'
+export const GREEN_COLOR = '#70c725'
+
+//season URIs
+export const BIRTHDAY_SEASONS = {
+    SUMMER: 'https://stardewvalleywiki.com/mediawiki/images/8/85/Summer.png',
+    SPRING: 'https://stardewvalleywiki.com/mediawiki/images/9/9c/Spring.png',
+    FALL: 'https://stardewvalleywiki.com/mediawiki/images/5/5d/Fall.png',
+    WINTER: 'https://stardewvalleywiki.com/mediawiki/images/a/a7/Winter.png',
+}
